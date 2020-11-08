53fc6fe3bc453ad92135c75e290f248772278328
Dan Smith <dan.smith@theappbusiness.com>
2020-11-08 09:19:45 +0000

    Adds Framework model and mockdata to the project

---
+++ Apple-Frameworks/Apple-Frameworks/Framework.swift
@@ -0,0 +1,15 @@
+//
+//  Framework.swift
+//  Apple-Frameworks
+//
+//  Created by Dan Smith on 08/11/2020.
+//
+
+import Foundation
+
+struct Framework {
+  let name: String
+  let imageName: String
+  let urlString: String
+  let description: String
+}
