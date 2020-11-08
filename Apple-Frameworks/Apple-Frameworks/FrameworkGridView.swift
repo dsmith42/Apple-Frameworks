594424e9132a2ce6ab9dbd9bf1b3f7f9b82ecb86
Dan Smith <dan.smith@theappbusiness.com>
2020-11-08 10:02:11 +0000

    Implements a simple scrollable grid view.

--- Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -8,23 +8,49 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
-    var body: some View {
-
-      VStack(spacing: 10) {
-        Image(MockData.sampleFramework.imageName)
-          .resizable()
-          .aspectRatio(contentMode: .fit)
-          .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-        Text(MockData.sampleFramework.name)
-          .lineLimit(1)
-          .scaledToFit()
-          .frame(width: 100, height: 50)
+  
+  let columns: [GridItem] = [GridItem(.flexible()),
+                             GridItem(.flexible()),
+                             GridItem(.flexible())]
+  
+  var body: some View {
+    
+    NavigationView {
+      ScrollView {
+        LazyVGrid(columns: columns) {
+          ForEach(MockData.frameworks) { framework in
+            FrameworkTitleView(framework: framework)
+          }
+        }
       }
+      .navigationTitle(" Frameworks")
     }
+  }
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
-    static var previews: some View {
-        FrameworkGridView()
+  static var previews: some View {
+    FrameworkGridView()
+      .preferredColorScheme(.dark)
+  }
+}
+
+struct FrameworkTitleView: View {
+  
+  let framework: Framework
+  
+  var body: some View {
+    VStack {
+      Image(framework.imageName)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 90, height: 90)
+      Text(framework.name)
+        .font(.title2)
+        .fontWeight(.semibold)
+        .scaledToFit()
+        .minimumScaleFactor(0.5)
     }
+    .padding()
+  }
 }
