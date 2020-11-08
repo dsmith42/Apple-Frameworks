047617f58a603a89f552e056917e03363ae18d22
Dan Smith <dan.smith@theappbusiness.com>
2020-11-08 09:01:52 +0000

    Initial commit

---
+++ Apple-Frameworks/Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Dan Smith on 08/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
